feacee8e274d749e37174efde4aaa6c846e14d21
Apeksha <apekshapriya@gmail.com>
2020-08-22 21:50:03 +0530

    added code for train, val loader, save model and eval code for accuracy of test set

--- src/train.py
+++ src/train.py
@@ -4,18 +4,22 @@ import torchvision.transforms as transforms
 from absl import app
 from absl import flags
 from absl import logging
-
+import os
 from resnet import Net
 
+torch.manual_seed(0)
 
 FLAGS = flags.FLAGS
 
 flags.DEFINE_string("data_path", "../data", "Path to store dataset")
 flags.DEFINE_boolean("debug", False, "Runs in debug mode")
-flags.DEFINE_integer("batch_size", 4, "Batch size")
-flags.DEFINE_integer("epochs", 2, "Number of epochs to run the training")
+flags.DEFINE_integer("batch_size", 256, "Batch size")
+flags.DEFINE_integer("epochs", 3, "Number of epochs to run the training")
 flags.DEFINE_float("learning_rate", 0.001, "Learning rate")
+flags.DEFINE_string("save_model_dir", "../model", "Path to dir to save model")
 
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 def download_data(data_path, transform=None):
     """
@@ -23,22 +27,27 @@ def download_data(data_path, transform=None):
     Doesn't download if it already exists.
     """
     # Get the CIFAR 10 data
-    trainset = torchvision.datasets.CIFAR10(root=data_path, train=True,
+    dataset = torchvision.datasets.CIFAR10(root=data_path, train=True,
                                             transform=transform, download=True)
+
+    trainset, valset = torch.utils.data.random_split(dataset, [45000, 5000])
+
     testset = torchvision.datasets.CIFAR10(root=data_path, train=False,
                                            transform=transform, download=True)
 
+
     logging.debug(trainset)
+    logging.debug(valset)
     logging.debug(testset)
 
     trainloader = torch.utils.data.DataLoader(trainset,
                                               batch_size=FLAGS.batch_size,
                                               shuffle=True, num_workers=1)
-    testloader = torch.utils.data.DataLoader(testset,
+    valloader = torch.utils.data.DataLoader(valset,
                                              batch_size=FLAGS.batch_size,
-                                             shuffle=True, num_workers=1)
+                                             shuffle=False, num_workers=1)
 
-    return trainloader, testloader
+    return trainloader, valloader
 
 
 def get_transform():
@@ -62,12 +71,17 @@ def main(argv):
     else:
         logging.set_verbosity(logging.INFO)
 
+    if not os.path.exists(FLAGS.save_model_dir):
+        os.makedirs(FLAGS.save_model_dir)
+
     # Transform and Load the dataset
     transform = get_transform()
-    trainloader, testloader = download_data(FLAGS.data_path, transform)
+    trainloader, valloader = download_data(FLAGS.data_path, transform)
 
     # Get the model
     model = Net()
+    if torch.cuda.is_available():
+        model = model.to(device)
 
     # Create the loss fn and optimizer
     loss_fn = torch.nn.CrossEntropyLoss()
@@ -81,6 +95,9 @@ def main(argv):
             # Take the inputs and labels
             inputs, labels = data
 
+            if torch.cuda.is_available():
+                inputs = inputs.to(device)
+                labels = labels.to(device)
             # Make sure the grads are zero before a step
             optimizer.zero_grad()
 
@@ -97,6 +114,41 @@ def main(argv):
 
         avg_loss = sum(epoch_loss) / len(epoch_loss) 
         logging.info(f"Epoch: {epoch}\tAverage Loss: {avg_loss}")
+
+        # get val loss
+        val_epoch_loss = []
+        last_val_loss = 100
+        for i, data in enumerate(valloader):
+
+            # Take the inputs and labels
+            inputs, labels = data
+            if torch.cuda.is_available():
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+            # Do a backprop
+            outputs = model(inputs)
+
+            loss = loss_fn(outputs, labels)
+
+            # Log statistics
+            val_epoch_loss.append(loss.item())
+
+
+        val_avg_loss = sum(val_epoch_loss) / len(val_epoch_loss)
+        logging.info(f"Epoch: {epoch}\tAverage Val Loss: {val_avg_loss}")
+
+        if val_avg_loss<last_val_loss:
+
+            torch.save({
+                'epoch': epoch,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'loss': val_avg_loss},  os.path.join(FLAGS.save_model_dir, "model_{}.pth".format(epoch)))
+            last_val_loss = val_avg_loss
+
+
+    "------------------------FINISHED TRAINING--------------------------"
 
 
 if __name__ == "__main__":
