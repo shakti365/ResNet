2416db012fc3eed318153b887795e04aa2a5d772
Shivam Shakti <shivam.shakti365@gmail.com>
2020-08-23 12:11:43 +0530

    Add tensorboard logging and Update requirement

--- src/train.py
+++ src/train.py
@@ -6,9 +6,13 @@ from absl import flags
 from absl import logging
 import os
 from resnet import Net
-from matplotlib import pyplot as plt
-torch.manual_seed(0)
+from torch.utils.tensorboard import SummaryWriter
+from datetime import datetime
 
+
+torch.manual_seed(0)
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model_name = str(datetime.now())[:-3].replace(' ', '-')
 FLAGS = flags.FLAGS
 
 flags.DEFINE_string("data_path", "../data", "Path to store dataset")
@@ -16,12 +20,11 @@ flags.DEFINE_boolean("debug", False, "Runs in debug mode")
 flags.DEFINE_integer("batch_size", 256, "Batch size")
 flags.DEFINE_integer("epochs", 3, "Number of epochs to run the training")
 flags.DEFINE_float("learning_rate", 0.001, "Learning rate")
-flags.DEFINE_string("save_model_dir", "../model", "Path to dir to save model")
-flags.DEFINE_string("save_log_dir", "../logs", "Path to dir to save logs")
+flags.DEFINE_string("save_model_dir",
+                    f"../models/{model_name}", "Path to dir to save model")
+flags.DEFINE_string("save_log_dir", 
+                    f"../logs/{model_name}", "Path to dir to save logs")
 
-
-
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 def download_data(data_path, transform=None):
     """
@@ -67,6 +70,18 @@ def get_transform():
     return transform
 
 
+def log_vars_and_grads(writer, variables, step):
+    """
+    Logs variables and their gradients to tensorboard
+    """
+    for i, var in enumerate(variables):
+        if var.grad is None:
+            continue
+        else:
+            writer.add_histogram(f"var_{i}", var, step)
+            writer.add_histogram(f"grad_{i}", var.grad, step)
+
+
 def main(argv):
     if FLAGS.debug:
         logging.set_verbosity(logging.DEBUG)
@@ -78,6 +93,10 @@ def main(argv):
 
     if not os.path.exists(FLAGS.save_log_dir):
         os.makedirs(FLAGS.save_log_dir)
+
+    # Create the summary writer for tensorboard
+    train_writer = SummaryWriter(FLAGS.save_log_dir + "/train")
+    valid_writer = SummaryWriter(FLAGS.save_log_dir + "/valid")
 
     # Transform and Load the dataset
     transform = get_transform()
@@ -92,8 +111,6 @@ def main(argv):
     loss_fn = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=FLAGS.learning_rate)
 
-    train_loss = []
-    val_loss = []
     for epoch in range(FLAGS.epochs):
 
         epoch_loss = []
@@ -101,6 +118,11 @@ def main(argv):
 
             # Take the inputs and labels
             inputs, labels = data
+
+            # Log the model graph only once
+            # Since it needs the inputs, we have to put this condn
+            if epoch == 0 and i == 0:
+                train_writer.add_graph(model, inputs)
 
             if torch.cuda.is_available():
                 inputs = inputs.to(device)
@@ -118,16 +140,15 @@ def main(argv):
             epoch_loss.append(loss.item())
             if i % 1000 == 0:
                 logging.info(f"Epoch: {epoch}\tIteration: {i}\tLoss: {loss}")
+            break
 
-        avg_loss = sum(epoch_loss) / len(epoch_loss) 
-        logging.info(f"Epoch: {epoch}\tAverage Loss: {avg_loss}")
-
-        train_loss.append(avg_loss)
-        plt.xlabel("Epoch")
-        plt.ylabel('Train loss')
-        plt.plot([i for i in range(epoch+1)],train_loss)
-        plt.savefig(os.path.join(FLAGS.save_log_dir,'Train_loss.jpg'))
-        plt.close()
+        # Log train set metrics
+        train_avg_loss = sum(epoch_loss) / len(epoch_loss)
+        logging.info(f"Epoch: {epoch}\tAverage Train Loss: {train_avg_loss}")
+        train_writer.add_scalar("loss",
+                                train_avg_loss,
+                                epoch)
+        log_vars_and_grads(train_writer, model.parameters(), epoch)
 
         # get val loss
         val_epoch_loss = []
@@ -147,10 +168,14 @@ def main(argv):
 
             # Log statistics
             val_epoch_loss.append(loss.item())
+            break
 
-
+        # Log validation set metrics
         val_avg_loss = sum(val_epoch_loss) / len(val_epoch_loss)
         logging.info(f"Epoch: {epoch}\tAverage Val Loss: {val_avg_loss}")
+        valid_writer.add_scalar("loss",
+                                val_avg_loss,
+                                epoch)
 
         if val_avg_loss<last_val_loss:
 
@@ -161,14 +186,6 @@ def main(argv):
                 'loss': val_avg_loss},  os.path.join(FLAGS.save_model_dir, "model_{}.pth".format(epoch)))
             last_val_loss = val_avg_loss
 
-        val_loss.append(val_avg_loss)
-        plt.xlabel("Epoch")
-        plt.ylabel('Val loss')
-        plt.plot([i+1 for i in range(epoch+1)], val_loss)
-        plt.savefig(os.path.join(FLAGS.save_log_dir,'Val_loss.jpg'))
-        plt.close()
-
-    "------------------------FINISHED TRAINING--------------------------"
 
 
 if __name__ == "__main__":
